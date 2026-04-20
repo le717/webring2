e3a87f0ee09324dd0f2d4193d08619964c1c428f
Caleb Ely <le717@users.noreply.github.com>
2026-04-20 17:36:17 -0400

    remove db

--- webring/settings/local.py
+++ webring/settings/local.py
@@ -6,7 +6,7 @@ from .base import BASE_DIR
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "webring.db",
     }
 }
 SITE_TITLE = "Webring"
