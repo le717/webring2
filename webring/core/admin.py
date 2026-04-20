da69cd39b4506d6d1ac8fd9a542f1f7df174c073
Caleb Ely <le717@users.noreply.github.com>
2026-04-20 19:59:52 -0400

    Make models and admin

--- webring/core/admin.py
+++ webring/core/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Entry, History, Webring
+
+
+@admin.register(Webring)
+class WebringAdmin(admin.ModelAdmin):
+    list_display = ["name", "url", "author", "maintainer", "is_active"]
+    list_filter = ["is_active"]
+    readonly_fields = ["uuid"]
+    ordering = ["name", "is_active"]
+    search_fields = ["name", "url", "author", "maintainer"]
+    search_help_text = "Search by webring name, url, author, or maintainer."
+
+
+class EntryHistoryInline(admin.TabularInline):
+    model = History
+
+    def has_add_permission(self, *args, **kwargs) -> bool:
+        return False
+
+    def has_change_permission(self, *args, **kwargs) -> bool:
+        return False
+
+    def has_delete_permission(self, *args, **kwargs) -> bool:
+        return False
+
+
+@admin.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    inlines = [EntryHistoryInline]
+    list_display = ["title", "url", "is_dead", "is_web_archive", "instance"]
+    fields = ["instance", "url", "title", "description", "is_dead", "is_web_archive"]
+    search_fields = ["title", "description", "url"]
+    readonly_fields = ["uuid"]
+    autocomplete_fields = ["instance"]
+    ordering = ["instance", "title"]
+    search_help_text = "Search by entry title, description, or URL."
+
+
+# @admin.register(History)
+# class HistoryAdmin(admin.ModelAdmin):
+#     def has_add_permission(self, *args, **kwargs) -> bool:
+#         return False
+
+#     def has_change_permission(self, *args, **kwargs) -> bool:
+#         return False
+
+#     def has_delete_permission(self, *args, **kwargs) -> bool:
+#         return False
--- webring/core/models.py
+++ webring/core/models.py
@@ -1,3 +1,116 @@
-from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+__all__ = ["Entry", "History", "Webring"]
+
+
+class Webring(models.Model):
+    """Represent a single webring instance."""
+
+    class Meta:
+        db_table_comment = _("Store the individual webring instances being run on this app.")
+
+    def __str__(self) -> str:
+        return f"{self.name} ({self.url})"
+
+    uuid = models.UUIDField(
+        unique=True,
+        default=uuid4,
+        verbose_name="UUID",
+        help_text=_(
+            "A unique UUID for this webring. This is auto-generated and cannot be changed."
+        ),
+    )
+    name = models.CharField(max_length=512, help_text=_("The webring's name."))
+    url = models.URLField(verbose_name="URL", help_text=_("The URL of the webring."))
+    author = models.CharField(
+        max_length=512, blank=True, default="", help_text=_("The primary author of the webring.")
+    )
+    maintainer = models.CharField(
+        max_length=512,
+        blank=True,
+        default="",
+        help_text=_("The primary maintainer of the maintainer."),
+    )
+    api_key = models.CharField(
+        max_length=512,
+        blank=True,
+        default=uuid4,
+        verbose_name="Admin API key",
+        help_text="The admin API key for accessing protected routes.",
+    )
+    is_active = models.BooleanField(
+        default=True,
+        help_text="Indicate if this webring is live, meaning it can be viewed and traversed.",
+    )
+
+
+class Entry(models.Model):
+    class Meta:
+        verbose_name_plural = "Entries"
+        db_table_comment = _("Store the entries for an individual webring.")
+
+    def __str__(self) -> str:
+        return self.title
+
+    uuid = models.UUIDField(
+        unique=True,
+        default=uuid4,
+        verbose_name="UUID",
+        help_text=_("A unique UUID for this entry. This is auto-generated and cannot be changed."),
+    )
+    title = models.CharField(max_length=2_048, help_text=_("The entry's title."))
+    description = models.CharField(
+        max_length=1_024,
+        blank=True,
+        default="",
+        help_text=_("The entry's description. Should be short(er)."),
+    )
+    url = models.URLField(verbose_name="URL", help_text=_("The entry's URL."))
+    date_added = models.DateTimeField(
+        auto_now_add=True, help_text=_("The datetime this entry was added to the webring.")
+    )
+    date_last_updated = models.DateTimeField(
+        auto_now=True,
+        help_text=_(
+            "The most recent datetime this entry was updated.",
+        ),
+    )
+    is_dead = models.BooleanField(
+        default=False,
+        verbose_name="Entry is dead?",
+        help_text=_("Indicate if this entry is dead (i.e., cannot be accessed anymore.)"),
+    )
+    is_web_archive = models.BooleanField(
+        default=False,
+        verbose_name="Entry redirects to Web Archive?",
+        help_text=_("Indicate if this entry's URL is now pointed to The Web Archive."),
+    )
+    instance = models.ForeignKey(
+        Webring,
+        on_delete=models.CASCADE,
+        related_name="entries",
+        help_text=_("The webring this entry belong to."),
+    )
+
+
+class History(models.Model):
+    class Meta:
+        verbose_name = "Entry history"
+        verbose_name_plural = "Entry histories"
+        db_table_comment = _("Audit log of linkrot checks.")
+
+    date_added = models.DateTimeField(
+        auto_now_add=True, help_text=_("The datetime this check occurred.")
+    )
+    url = models.URLField(help_text=_("The URL checked."))
+    was_alive = models.BooleanField(
+        default=True,
+        verbose_name="URL was alive?",
+        help_text=_("Was the URL alive at the time of this check?"),
+    )
+    message = models.TextField(default="", help_text="Any message generated during the check.")
+    entry = models.ForeignKey(Entry, on_delete=models.CASCADE, related_name="history")
