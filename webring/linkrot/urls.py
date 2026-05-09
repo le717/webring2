f5535b139ff6f5a824296c41a1181557371ad236
Caleb Ely <le717@users.noreply.github.com>
2026-05-09 14:22:13 -0400

    stub in linkrot routes

---
+++ webring/linkrot/urls.py
@@ -0,0 +1,10 @@
+# from django.urls import path
+
+# from . import views
+
+app_name = "linkrot"
+urlpatterns = [
+    # path("<slug:ring>/linkrot", views.WebringListView.as_view(), name="check_all"),
+    # path("<slug:ring>/link/<uuid:entry>", views.EntryView.as_view(), name="check_one"),
+    # path("<slug:ring>/link/<uuid:entry>/history", views.EntryView.as_view(), name="history"),
+]
--- webring/urls.py
+++ webring/urls.py
@@ -22,5 +22,6 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("embed/", include("webring.embed.urls", namespace="embed")),
+    path("linkrot/", include("webring.linkrot.urls", namespace="linkrot")),
     path("", include("webring.core.urls", namespace="core")),
 ]
